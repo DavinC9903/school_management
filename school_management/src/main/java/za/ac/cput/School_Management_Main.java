8c8fc54d924d725625acd8285091520e9fa32df0
Nolubabalo Ndongeni <nndongeni89@gmail.com>
2022-06-14 18:06:56 +0100

    Pom file updated Classes moved to their packages

--- school_management/src/main/java/za/ac/cput/School_Management_Main.java
+++ school_management/src/main/java/za/ac/cput/School_Management_Main.java
@@ -1,7 +1,6 @@
 package za.ac.cput;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class School_Management_Main {
     public static void main(String[] args) {
--- school_management/src/test/java/za/ac/cput/factory/details/Impl/CityFactoryTest.java
+++ school_management/src/test/java/za/ac/cput/factory/details/Impl/CityFactoryTest.java
@@ -1,4 +1,4 @@
-package za.ac.cput.factory;
+package za.ac.cput.factory.details.Impl;
 
 import org.junit.jupiter.api.Test;
 import za.ac.cput.domain.details.City;
