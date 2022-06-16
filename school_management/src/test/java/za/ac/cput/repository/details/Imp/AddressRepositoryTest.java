34d748d046fc41e642ca488ba8eb66133cc8f983
Chuma Nxazonke <219181187@mycput.ac.za>
2022-06-16 06:58:49 +0200

    Address entity have been implemented using builder patter, factory method and repository method.

---
+++ school_management/src/test/java/za/ac/cput/repository/details/Imp/AddressRepositoryTest.java
@@ -0,0 +1,58 @@
+//This is a test class for addess repository
+
+package za.ac.cput.repository.details.Imp;
+import org.junit.jupiter.api.*;
+import za.ac.cput.factory.details.AddressFactory;
+import za.ac.cput.domain.details.Address;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Chuma Nxazonke
+ * Student number: 219181187
+ * Date: 13 June 2022
+ */
+
+class AddressRepositoryTest {
+
+    private Address address;
+    private AddressRepository repository;
+
+    @BeforeEach
+    void setup() {
+        this.address = AddressFactory.createAddress("AD25", "Plain street", "2023", "Plain street", 8001);
+        this.repository = AddressRepository.addressRepository();
+        Address saved = this.repository.save(this.address);
+        assertSame(this.address, saved);
+
+        System.out.println("Data successfully saved in the database");
+
+    }
+
+    @AfterEach
+    void tearDown(){
+
+        this.repository.delete(this.address);
+        List<Address> addressList = this.repository.findAll();
+        assertEquals(0, addressList.size());
+
+        System.out.println("Record successfully deleted the information");
+
+    }
+
+@Test
+    void read(){
+    Optional<Address> read = this.repository.read(this.address.getUnitNumber());
+    assertTrue(read.isPresent());
+    assertSame(this.address, read.get());
+}
+
+@Test
+    void findAll(){
+        List<Address> addressList = this.repository.findAll();
+        assertEquals(1, addressList.size());
+}
+
+}
