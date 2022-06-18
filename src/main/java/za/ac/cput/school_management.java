6c9d6ec69ffa6685b42bccf6aed4c25287d0f26f
duncanharris <duncanharris12@gmail.com>
2022-06-18 16:22:17 +0200

    Skeleton code, Project pom correction.

---
+++ src/main/java/za/ac/cput/school_management.java
@@ -0,0 +1,14 @@
+package za.ac.cput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class school_management {
+    public static void main(String[] args) {
+        SpringApplication.run(school_management.class, args);
+    }
+}
